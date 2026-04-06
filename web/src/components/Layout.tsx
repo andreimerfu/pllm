263e2e9c9c81a30f8f5cb87b905755be160a3880
Andrei <andreimerfu@outlook.com>
2026-04-06 11:41:12 +0300

    fix: remove bottom padding from main layout so sticky bars sit flush

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <SidebarInset>
         <AppNavbar />
-        <main className="flex flex-1 flex-col gap-4 bg-background p-6 lg:p-8">
+        <main className="flex flex-1 flex-col gap-4 bg-background px-6 pt-6 pb-0 lg:px-8 lg:pt-8">
           <div className="w-full flex-1 flex flex-col min-h-0">
             {children}
           </div>
